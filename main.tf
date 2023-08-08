b8f5220038b7ae677bf14b4a55d6c606ea8d96a5
Rakshith Raghavendra Kashyap <58388399+kashyaprakshith@users.noreply.github.com>
2023-08-08 11:58:37 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,20 +7,26 @@ terraform {
   }
 }
 
-provider "virtualbox" {
-  version = "1.15.2"  # Replace with the desired provider version
-}
+# There are currently no configuration options for the provider itself.
 
 resource "virtualbox_vm" "node" {
-  count = 2
-  name  = "${format("node-%02d", count.index + 1)}"
-  url   = "https://vagrantcloud.com/ubuntu/boxes/xenial64/versions/20180420.0.0/providers/virtualbox.box"
-  image = "./virtualbox.box"
-  cpus  = 2
-  memory = "512 mib"
+  count     = 2
+  name      = format("node-%02d", count.index + 1)
+  image     = "https://app.vagrantup.com/ubuntu/boxes/bionic64/versions/20180903.0.0/providers/virtualbox.box"
+  cpus      = 2
+  memory    = "512 mib"
+  user_data = file("${path.module}/user_data")
 
   network_adapter {
-    type = "bridged"
-    host_interface = "en0"
+    type           = "hostonly"
+    host_interface = "vboxnet1"
   }
+}
+
+output "IPAddr" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 1)
+}
+
+output "IPAddr_2" {
+  value = element(virtualbox_vm.node.*.network_adapter.0.ipv4_address, 2)
 }
