430122afafd1f834f189851f4bf484cac7b60022
Rakshith Raghavendra Kashyap <58388399+kashyaprakshith@users.noreply.github.com>
2023-08-08 11:44:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
-provider "virtualbox" {}
+provider "virtualbox" {
+  version = "1.15.2"  # Replace with the desired provider version
+}
 
 resource "virtualbox_vm" "example_vm" {
   name   = "my-vm"
@@ -6,8 +8,8 @@ resource "virtualbox_vm" "example_vm" {
 }
 
 resource "virtualbox_nic" "example_nic" {
-  vm_id     = virtualbox_vm.example_vm.id
-  nic_type  = "virtio"
-  host_port = 8080
+  vm_id      = virtualbox_vm.example_vm.id
+  nic_type   = "virtio"
+  host_port  = 8080
   guest_port = 80
 }
